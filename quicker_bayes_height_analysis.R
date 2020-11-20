23faf26a55b48e9e7e65cdc1303cd06844a382af
Caroline <mckeonc2@tcd.ie>
2020-11-20 21:57:08 +0000

    have the script, but need to rerun the bayesian models with larger effective sample size. phylogeny not converging (but i can do a much bigger sample size easily. will just take a while)

--- quicker_bayes_height_analysis.R
+++ quicker_bayes_height_analysis.R
@@ -1,92 +1,94 @@
 ## quicker bayes height analysis
 
 
-m_indiv_bayes <- list()
-#for(i in names(comp_data[["data"]][which(names(comp_data[["data"]]) %in% c("mean", "median", "max", "coeff_var"))])){
+# m_indiv_bayes <- list()
+# #for(i in names(comp_data[["data"]][which(names(comp_data[["data"]]) %in% c("mean", "median", "max", "coeff_var"))])){
+# 
+#   m_indiv_bayes[["coeff_var"]][["total.area"]] <-mod_list <- mclapply(1:2, function(i) {
+#   MCMCglmm(fixed = log(total.area) ~ log(coeff_var),
+#            random = ~ animal,
+#            rcov = ~units,
+#            family= "gaussian",
+#            pedigree = comp_data$tree,
+#            data = comp_data$data,
+#            nitt = nitt,
+#            burnin = burnin,
+#            thin = thin,
+#            prior = prior)
+# }, mc.cores=2)
+# 
+# m_indiv_bayes[["coeff_var"]][["prop.landscape"]] <-mod_list <- mclapply(1:2, function(i) {
+#   MCMCglmm(fixed = log(prop.landscape) ~ log(coeff_var),
+#            random = ~ animal,
+#            rcov = ~units,
+#            family= "gaussian",
+#            pedigree = comp_data$tree,
+#            data = comp_data$data,
+#            nitt = nitt,
+#            burnin = burnin,
+#            thin = thin,
+#            prior = prior)
+# }, mc.cores=2)
+# 
+# m_indiv_bayes[["coeff_var"]][["perimeter.area.frac.dim"]] <-mod_list <- mclapply(1:2, function(i) {
+#   MCMCglmm(fixed = log(perimeter.area.frac.dim) ~ log(coeff_var),
+#            random = ~ animal,
+#            rcov = ~units,
+#            family= "gaussian",
+#            pedigree = comp_data$tree,
+#            data = comp_data$data,
+#            nitt = nitt,
+#            burnin = burnin,
+#            thin = thin,
+#            prior = prior)
+# }, mc.cores=2)
+# 
+# m_indiv_bayes[["coeff_var"]][["mean.shape.index"]] <-mod_list <- mclapply(1:2, function(i) {
+#   MCMCglmm(fixed = log(mean.shape.index) ~ log(coeff_var),
+#            random = ~ animal,
+#            rcov = ~units,
+#            family= "gaussian",
+#            pedigree = comp_data$tree,
+#            data = comp_data$data,
+#            nitt = nitt,
+#            burnin = burnin,
+#            thin = thin,
+#            prior = prior)
+# }, mc.cores=2)
+# 
+# m_indiv_bayes[["coeff_var"]][["effective.mesh.size"]] <-mod_list <- mclapply(1:2, function(i) {
+#   MCMCglmm(fixed = log(effective.mesh.size) ~ log(coeff_var),
+#            random = ~ animal,
+#            rcov = ~units,
+#            family= "gaussian",
+#            pedigree = comp_data$tree,
+#            data = comp_data$data,
+#            nitt = nitt,
+#            burnin = burnin,
+#            thin = thin,
+#            prior = prior)
+# }, mc.cores=2)
+# 
+# m_indiv_bayes[["coeff_var"]][["range.size"]] <-mod_list <- mclapply(1:2, function(i) {
+#   MCMCglmm(fixed = log(range.size) ~ log(coeff_var),
+#            random = ~ animal,
+#            rcov = ~units,
+#            family= "gaussian",
+#            pedigree = comp_data$tree,
+#            data = comp_data$data,
+#            nitt = nitt,
+#            burnin = burnin,
+#            thin = thin,
+#            prior = prior)
+# }, mc.cores=2) 
 
-  m_indiv_bayes[["coeff_var"]][["total.area"]] <-mod_list <- mclapply(1:2, function(i) {
-  MCMCglmm(fixed = log(total.area) ~ log(coeff_var),
-           random = ~ animal,
-           rcov = ~units,
-           family= "gaussian",
-           pedigree = comp_data$tree,
-           data = comp_data$data,
-           nitt = nitt,
-           burnin = burnin,
-           thin = thin,
-           prior = prior)
-}, mc.cores=2)
-
-m_indiv_bayes[["coeff_var"]][["prop.landscape"]] <-mod_list <- mclapply(1:2, function(i) {
-  MCMCglmm(fixed = log(prop.landscape) ~ log(coeff_var),
-           random = ~ animal,
-           rcov = ~units,
-           family= "gaussian",
-           pedigree = comp_data$tree,
-           data = comp_data$data,
-           nitt = nitt,
-           burnin = burnin,
-           thin = thin,
-           prior = prior)
-}, mc.cores=2)
-
-m_indiv_bayes[["coeff_var"]][["perimeter.area.frac.dim"]] <-mod_list <- mclapply(1:2, function(i) {
-  MCMCglmm(fixed = log(perimeter.area.frac.dim) ~ log(coeff_var),
-           random = ~ animal,
-           rcov = ~units,
-           family= "gaussian",
-           pedigree = comp_data$tree,
-           data = comp_data$data,
-           nitt = nitt,
-           burnin = burnin,
-           thin = thin,
-           prior = prior)
-}, mc.cores=2)
-
-m_indiv_bayes[["coeff_var"]][["mean.shape.index"]] <-mod_list <- mclapply(1:2, function(i) {
-  MCMCglmm(fixed = log(mean.shape.index) ~ log(coeff_var),
-           random = ~ animal,
-           rcov = ~units,
-           family= "gaussian",
-           pedigree = comp_data$tree,
-           data = comp_data$data,
-           nitt = nitt,
-           burnin = burnin,
-           thin = thin,
-           prior = prior)
-}, mc.cores=2)
-
-m_indiv_bayes[["coeff_var"]][["effective.mesh.size"]] <-mod_list <- mclapply(1:2, function(i) {
-  MCMCglmm(fixed = log(effective.mesh.size) ~ log(coeff_var),
-           random = ~ animal,
-           rcov = ~units,
-           family= "gaussian",
-           pedigree = comp_data$tree,
-           data = comp_data$data,
-           nitt = nitt,
-           burnin = burnin,
-           thin = thin,
-           prior = prior)
-}, mc.cores=2)
-
-m_indiv_bayes[["coeff_var"]][["range.size"]] <-mod_list <- mclapply(1:2, function(i) {
-  MCMCglmm(fixed = log(range.size) ~ log(coeff_var),
-           random = ~ animal,
-           rcov = ~units,
-           family= "gaussian",
-           pedigree = comp_data$tree,
-           data = comp_data$data,
-           nitt = nitt,
-           burnin = burnin,
-           thin = thin,
-           prior = prior)
-}, mc.cores=2) 
 
 
-
-save <- m_indiv_bayes
+#save <- m_indiv_bayes
 #saveRDS(m_indiv_bayes, "quick_bayes_height_phylo.rds")
 #m_indiv_bayes[["median"]] <- m_indiv_bayes[["median"]][-7]
+
+m_indiv_bayes <- readRDS("quick_bayes_height_phylo.rds")
 
 par(mfrow = c(2,3))
 
@@ -107,34 +109,38 @@ c<- cbind(c[-1,], one)
 
 par(mfrow = c(2,3))
 for(i in names(mydata[which(names(mydata) %in% c("effective.mesh.size", "mean.shape.index", "prop.landscape", "total.area", "perimeter.area.frac.dim", "range.size"))])) {
-   plot(log(mydata[,i]) ~ log(height), data = mydata)
+   plot(log(mydata[,i]) ~ log(height), data = mydata, col = "dark grey", bty = "n")
+  abline(c[,1][c$hh== "mean" & c$rr ==i], c[,2][c$hh== "mean" & c$rr ==i], col = "red", lwd = 4)
+  abline(c[,1][c$hh== "median" & c$rr ==i], c[,2][c$hh== "median" & c$rr ==i], col = "blue", lwd = 4)
+  abline(c[,1][c$hh== "max" & c$rr ==i], c[,2][c$hh== "max" & c$rr ==i], col = "green", lwd = 4)
+  abline(c[,1][c$hh== "coeff_var" & c$rr ==i], c[,2][c$hh== "coeff_var" & c$rr ==i], col = "purple", lwd = 4)
   }
 
-for(j in h){
-  for(i in r) {
-    abline(c$"a"[c$hh== j & c$rr ==i], c$"b"[c$hh== j & c$rr ==i])}}
+# for(j in unique(levels(c$rr))){
+#   for(i in unique(levels(c$hh))) {
+#     abline(c[,1][c$hh== "mean" & c$rr ==j], c[,2][c$hh== i & c$rr ==j])
+#     }}
+# 
+# for(i in names(mydata[which(names(mydata) %in% c("effective.mesh.size", "mean.shape.index", "prop.landscape", "total.area", "perimeter.area.frac.dim", "range.size"))])){
+#   plot(log(mydata[,i]) ~ log(height), data = mydata, main = paste(i), ylab = paste("log", i, sep = " "), col = "grey", bty = "n")
+#   abline(m_indiv[["mean"]][[i]], col = "red", lwd = 4)
+#   abline(m_indiv[["median"]][[i]], col = "blue", lwd = 4)
+#   abline(m_indiv[["max"]][[i]], col = "green", lwd = 4)
+#   abline(m_indiv[["coeff_var"]][[i]], col = "purple", lwd = 4)
+# }
+# 
+# abline(12, 1)
 
 
-for(i in names(mydata[which(names(mydata) %in% c("effective.mesh.size", "mean.shape.index", "prop.landscape", "total.area", "perimeter.area.frac.dim", "range.size"))])){
-  plot(log(mydata[,i]) ~ log(height), data = mydata, main = paste(i), ylab = paste("log", i, sep = " "), col = "grey", bty = "n")
-  abline(m_indiv[["mean"]][[i]], col = "red", lwd = 4)
-  abline(m_indiv[["median"]][[i]], col = "blue", lwd = 4)
-  abline(m_indiv[["max"]][[i]], col = "green", lwd = 4)
-  abline(m_indiv[["coeff_var"]][[i]], col = "purple", lwd = 4)
-}
-
-abline(12, 1)
-
-
-x <- MCMCglmm(fixed = log(total.area) ~ log(mean),
-              random = ~ animal,
-         rcov = ~units,
-         family= "gaussian",
-         pedigree = comp_data$tree,
-         data = comp_data$data,
-         nitt = nitt,
-         burnin = burnin,
-         thin = thin,
-         prior = prior)
+# x <- MCMCglmm(fixed = log(total.area) ~ log(mean),
+#               random = ~ animal,
+#          rcov = ~units,
+#          family= "gaussian",
+#          pedigree = comp_data$tree,
+#          data = comp_data$data,
+#          nitt = nitt,
+#          burnin = burnin,
+#          thin = thin,
+#          prior = prior)
 
 
