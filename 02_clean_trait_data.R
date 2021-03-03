d2a8d70b4f30d0ad1627d8f9ffb252c2e43a3df0
Caroline <mckeonc2@tcd.ie>
2021-03-03 17:51:31 +0000

    cleaned continous traits

--- 02_clean_trait_data.R
+++ 02_clean_trait_data.R
@@ -33,7 +33,7 @@ mydata <- droplevels(mydata[mydata$trait_name %in% c("Seed dry mass","seed mass"
 # c("whole plant growth form", "Species tolerance to human impact", 
 #   "Species strategy type according to Grime", 
 #   "Species generation overlap",   "Mycorrhiza type", "leaf dry mass",   
-#    "Plant ontogeny: age of maturity (first flowering)", "plant flowering begin",)
+#    "Plant ontogeny: age of maturity (first flowering)", "plant flowering begin", "Dispersal unit dry mass")
 
 
 ## look at levels
@@ -90,39 +90,13 @@ mydata$species %>% unique(.) %>% length(.) ## 284 species
 for (i in names(mydata)){
   mydata[, i] <- as.character(mydata[,i])
 }
-
-seeds <- droplevels(mydata[mydata$trait_name %in% c("Seed dry mass","seed mass", "Dispersal unit dry mass"),])
-
-woody <- droplevels(mydata[mydata$trait_name %in% c("Plant woodiness", "whole plant woodiness"),])
-
-bank <- droplevels(mydata[mydata$trait_name %in% c("Seed (seedbank) longevity", "Seedbank type"),])
-
-dispersal <- droplevels(mydata[mydata$trait_name %in% c("Dispersal syndrome", "Dispersal unit appendages", 
-                                                        "Dispersal unit type", "Fruit type", "Seed morphology type"),])
-
-height <- droplevels(mydata[mydata$trait_name %in% c("Plant height generative", "Plant height vegetative", "maximum whole plant height"),])
-
-pollenation <- droplevels(mydata[mydata$trait_name %in% c("Flower insemination autogamous or xenogamous", 
-                                                          "Flower pollinator and type of reward", "Flower sexual syndrome (dichogamy, cleistogamy, dioecious, monoecious)", 
-                                                          "Pollination syndrome", "Plant mating system"),])
-
-sla <- droplevels(mydata[mydata$trait_name %in% c("Leaf area per leaf dry mass (specific leaf area, SLA or 1/LMA): petiole excluded", 
-                                                  "Leaf area per leaf dry mass (specific leaf area, SLA or 1/LMA): petiole included", 
-                                                  "Leaf area per leaf dry mass (specific leaf area, SLA or 1/LMA): undefined if petiole is in- or excluded",  
-                                                  "leaf area per leaf dry mass"),])
-
-dry_Nmass<- droplevels(mydata[mydata$trait_name %in% c("Leaf nitrogen (N) content per leaf dry mass",  "leaf nitrogen content per leaf dry mass"),])
-
-la <- droplevels(mydata[mydata$trait_name %in% c("leaf area"),])
-
-Nmass <- droplevels(mydata[mydata$trait_name %in% c("leaf nitrogen content per leaf area"),])
-
-clonal <- droplevels(mydata[mydata$trait_name %in% c("Plant propagation type", "Plant reproductive phenology timing", "Plant resprouting capacity", 
-                                                     "Plant vegetative regeneration capacity", "Plant vegetative reproduction: role of clonal growth organ in plant growth", 
-                                                     "Species reproduction type", "whole plant vegetative phenology", "Plant clonal growth form"),])
-
+mydata$species <- factor(mydata$species)
 
 ## clean the datasets
+
+## seedbank ----------------
+bank <- droplevels(mydata[mydata$trait_name %in% c("Seed (seedbank) longevity", "Seedbank type"),])
+
 bank$trait_name %>% factor(.) %>% table(.) %>% .[order(.)]
 dput(levels(factor(bank$trait_value)))
 
@@ -142,6 +116,9 @@ bank$trait_name <- "seed_bank"
 plot(factor(bank$StdValue))
 ## clean
 
+## dry_Nmass -------------
+dry_Nmass<- droplevels(mydata[mydata$trait_name %in% c("Leaf nitrogen (N) content per leaf dry mass",  "leaf nitrogen content per leaf dry mass"),])
+
 dry_Nmass$trait_name %>% factor(.) %>% table(.) %>% .[order(.)]
 dput(levels(factor(dry_Nmass$trait_value)))
 dry_Nmass$StdValue[which(is.na(dry_Nmass$StdValue))] <- dry_Nmass$trait_value[which(is.na(dry_Nmass$StdValue))] 
@@ -149,14 +126,126 @@ dry_Nmass$trait_name <- "Nmass_drymass"
 hist(as.numeric(dry_Nmass$StdValue))
 ## clean
 
+## SLA ------------
+sla <- droplevels(mydata[mydata$trait_name %in% c("Leaf area per leaf dry mass (specific leaf area, SLA or 1/LMA): petiole excluded", 
+                                                  "Leaf area per leaf dry mass (specific leaf area, SLA or 1/LMA): petiole included", 
+                                                  "Leaf area per leaf dry mass (specific leaf area, SLA or 1/LMA): undefined if petiole is in- or excluded",  
+                                                  "leaf area per leaf dry mass"),])
+
 sla$trait_name %>% factor(.) %>% table(.) %>% .[order(.)]
 dput(levels(factor(sla$trait_value)))
+sla$StdValue[which(is.na(sla$StdValue))] <- sla$trait_value[which(is.na(sla$StdValue))] 
+sla$StdValue <- as.numeric(sla$StdValue)
+
+par(mfrow = c(2,2))
+# hist(sla$StdValue[sla$trait_name == "Leaf area per leaf dry mass (specific leaf area, SLA or 1/LMA): petiole included"])
+# hist(sla$StdValue[sla$trait_name == "Leaf area per leaf dry mass (specific leaf area, SLA or 1/LMA): petiole excluded"&
+#                     sla$StdValue <600])
+# hist((sla$StdValue[sla$trait_name == "leaf area per leaf dry mass"]))
+# hist(sla$StdValue[sla$trait_name == "Leaf area per leaf dry mass (specific leaf area, SLA or 1/LMA): undefined if petiole is in- or excluded"])
+
+## not clean; has outliers...
+
+## woody ------
+woody <- droplevels(mydata[mydata$trait_name %in% c("Plant woodiness", "whole plant woodiness"),])
+
+woody$trait_name %>% factor(.) %>% table(.) %>% .[order(.)]
+dput(levels(factor(woody$trait_value)))
+woody$trait_value <- tolower(woody$trait_value)
+woody$trait_value[grep("h", woody$trait_value, ignore.case = TRUE)] <- "non-woody"
+woody$trait_value[woody$trait_value == "w"] <- "woody"
+woody$trait_value[woody$trait_value == "2" | woody$trait_value == "3"] <- "woody"
+woody <- woody[woody$trait_value %in% c("non-woody", "woody"),]
+woody$trait_name <- "woodiness"
+woody$StdValue <- woody$trait_value
+
+plot(factor(woody$StdValue))
+## clean 
+
+## seeds -----------
+seeds <- droplevels(mydata[mydata$trait_name %in% c("Seed dry mass","seed mass"),])
+
+seeds$trait_name %>% factor(.) %>% table(.) %>% .[order(.)]
+dput(levels(factor(seeds$trait_value)))
+seeds$StdValue[which(is.na(seeds$StdValue))] <- seeds$trait_value[which(is.na(seeds$StdValue))] 
+seeds <- seeds[seeds$StdValue != "heavy",]
+seeds$StdValue <- as.numeric(seeds$StdValue)
+hist(log(seeds$StdValue))
+seeds$trait_name <- "seed_mass"
+## clean
+
+## height ---------------
+height <- droplevels(mydata[mydata$trait_name %in% c("Plant height generative", "Plant height vegetative", "maximum whole plant height"),])
+
+height$trait_name %>% factor(.) %>% table(.) %>% .[order(.)]
+dput(levels(factor(height$StdValue)))
+height$StdValue[which(is.na(height$StdValue))] <- height$trait_value[which(is.na(height$StdValue))] 
+## removed "lower value dash" where a range of values was entered, leaving just the upper value
+dash <- c("[[:digit:]]-","[[:digit:]][[:digit:]]-","[[:digit:]][[:digit:]][[:digit:]]-", "[[:digit:]] - ","[[:digit:]][[:digit:]] - ", "[[:digit:]] -")
+height$StdValue <-gsub(paste(dash, collapse = "|"), "", height$StdValue)
+## removed brackets and keep higher values within
+brackets <- c("[[:digit:]] \\(", "\\(", "\\)")
+height$StdValue <-gsub(paste(brackets, collapse = "|"), "", height$StdValue)
+height$comment[grep("cm", height$StdValue)] <- "tofixcm"
+height$comment <- factor(height$comment)
+## remove text from the measurements
+words <- c("to ","cm", " m", "\\*", "<", " ")
+height$StdValue <-gsub(paste(words, collapse = "|"), "", height$StdValue)
+height <- height[height$StdValue %nin% c("herb", 0),]
+height$StdValue <-gsub(",", ".", height$StdValue)
+height$StdValue <- as.numeric(height$StdValue)
+height <- height[which(!is.na(height$StdValue)),]
+height$StdValue[height$comment %in% c("tofixcm")] <- height$StdValue[height$comment %in% c("tofixcm")]/100
+hist(height$StdValue)
+height$trait_name <- "height"
+## clean
+
+## la ----------
+la <- droplevels(mydata[mydata$trait_name %in% c("leaf area"),])
+
+la$trait_name %>% factor(.) %>% table(.) %>% .[order(.)]
+dput(levels(factor(la$trait_value)))
+la$trait_value <- as.numeric(la$trait_value)
+la <- la[la$comment != "leaf area x number of leaves",]
+la$StdValue <- la$trait_value
+hist(log(la$StdValue))
+## clean
+
+## Nmass -------------------
+Nmass <- droplevels(mydata[mydata$trait_name %in% c("leaf nitrogen content per leaf area"),])
+
+Nmass$trait_name %>% factor(.) %>% table(.) %>% .[order(.)]
+dput(levels(factor(Nmass$trait_value)))
+Nmass$StdValue <- Nmass$trait_value
+Nmass$StdValue <- as.numeric(Nmass$StdValue)
+hist(Nmass$StdValue)
+## clean
+
+
+
+
+
+
+
+## clonal --------
+clonal <- droplevels(mydata[mydata$trait_name %in% c("Plant propagation type", "Plant reproductive phenology timing", "Plant resprouting capacity", 
+                                                     "Plant vegetative regeneration capacity", "Plant vegetative reproduction: role of clonal growth organ in plant growth", 
+                                                     "Species reproduction type", "whole plant vegetative phenology", "Plant clonal growth form"),])
 
 clonal$trait_name %>% factor(.) %>% table(.) %>% .[order(.)]
 dput(levels(factor(clonal$trait_value)))
 
+
+dispersal <- droplevels(mydata[mydata$trait_name %in% c("Dispersal syndrome", "Dispersal unit appendages", 
+                                                        "Dispersal unit type", "Fruit type", "Seed morphology type"),])
+
+
 dispersal$trait_name %>% factor(.) %>% table(.) %>% .[order(.)]
 dput(levels(factor(dispersal$trait_value)))
+
+pollenation <- droplevels(mydata[mydata$trait_name %in% c("Flower insemination autogamous or xenogamous", 
+                                                          "Flower pollinator and type of reward", "Flower sexual syndrome (dichogamy, cleistogamy, dioecious, monoecious)", 
+                                                          "Pollination syndrome", "Plant mating system"),])
 
 pollenation$trait_name %>% factor(.) %>% table(.) %>% .[order(.)]
 dput(levels(factor(pollenation$trait_value)))
@@ -170,65 +259,10 @@ bank$trait_value[grep(paste(persistent, collapse="|"), bank$trait_value)] <- "pe
 
 
 
-####################################################################
-## Start of useful but unecessicary script where I clean "plant height" 
-## (previously named "trait_value"), even though TRY have already done it in StdValue...
-
-#levels(mydata$UnitName)
-## try hase pre-convert cm measurements to m
-
-#mydata$trait_value %>% factor(.) %>% table(.) %>% .[order(.)]
-
-## remove rows where the measurement value is nonsense
-mydata <- mydata[mydata$trait_value %nin% c("", ".", "]", "unknown"),]
-
-## remove text from the measurements
-words <- c("to ", " m", "\\*", "<")
-mydata$trait_value <-gsub(paste(words, collapse = "|"), "", mydata$trait_value)
+check<- rbind(bank, dry_Nmass, sla, woody, seeds, height, la, Nmass)
+check$species <- factor(check$species)
 
 
-length(which(is.na(mydata$trait_value)))
-## this changes when data is plant_height (character) is converted to numeric, so there must still be some non-numeric characters in there
-
-## removed "lower value dash" where a range of values was entered, leaving just the upper value
-dash <- c("[[:digit:]]-", "[[:digit:]] - ", "[[:digit:]] -")
-mydata$trait_value <-gsub(paste(dash, collapse = "|"), "", mydata$trait_value)
-
-## removed brackets and keep higher values within
-brackets <- c("[[:digit:]] \\(", "\\(", "\\)")
-mydata$trait_value <-gsub(paste(brackets, collapse = "|"), "", mydata$trait_value)
-
-## remove characters and convert out of ft to m (in theory, it's so messy I amn't sure I trust these values)
-check <- mydata[grep("ft", mydata$trait_value),]
-check$trait_value <-gsub(" ft", "", check$trait_value)
-check$trait_value <- as.numeric(check$trait_value)*0.3048
-mydata$trait_value[grep("ft", mydata$trait_value)] <- check$trait_value
-
-## do the same for "cm" data NOTE : also dropping lower set of digits befor a space
-check <- mydata[grep("cm", mydata$trait_value),]
-cm <- c(" cm", "cm")
-check$trait_value <-gsub(paste(cm, collapse = "|"), "", check$trait_value)
-## deal with entries that have a range of values and no dash
-sub_check <- check[grep("[[:digit:]] [[:digit:]]", check$trait_value),]
-sub_check$trait_value <-gsub("[[:digit:]] ", "", sub_check$trait_value)
-check$trait_value[grep("[[:digit:]] [[:digit:]]", check$trait_value)] <- sub_check$trait_value
-
-## proceed with cm data again
-check$trait_value <- as.numeric(check$trait_value)/100
-mydata$trait_value[grep("cm", mydata$trait_value)] <- check$trait_value
-
-rm(words, dash, brackets, cm)
-
-## look at the values we're left with
-summary(as.numeric(mydata$trait_value))
-
-# Min. 1st Qu.  Median    Mean 3rd Qu.    Max.    NA's
-#    0.00    0.80   10.00   52.12   30.00 6500.00     592
-
-
-####################################################################
-## End of useful but unecessicary script where I clean "plant height" 
-## (previously named "trait_value"), even though TRY have already done it in StdValue...
 
 mydata <- droplevels(unique(mydata))
 
@@ -237,6 +271,6 @@ hist((mydata$plant_height), main = "Histogram of plant heights", breaks = 1000)
 
 summary(as.numeric(mydata$plant_height))
 
-try_height <- mydata
+traits <- mydata
 
 ## the end 
